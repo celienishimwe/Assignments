6bc4a35a619a5587d14578280fbd35a17cdfda31
nishimwecelie <nishimwecelie@gmail.com>
2026-02-18 14:22:40 +0200

    added description on merging method

--- src/GenericAssignment2/GenericArrayUtility.java
+++ src/GenericAssignment2/GenericArrayUtility.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 
 public class GenericArrayUtility <T> {
 
+
+    /* this method is merging two arrays
+    * First it is applying error handling on null values if one is null it print other if all are null it print null array
+    * is using loop for traverse array and assign their values to merge array according to index we reached on
+    * for the second loop merge array it starts to the next index according to the size of previous array
+    * method is returning array values*/
+
     public static <T> T[] mergeArrays(T[] a, T[] b){
 
         T[] merge = (T[]) Array.newInstance(a.getClass().componentType(),a.length + b.length);
