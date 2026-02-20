9911af57fcb35853871d8cd1654be82a49cec776
nishimwecelie <nishimwecelie@gmail.com>
2026-02-20 12:23:15 +0200

    added output in format of map

--- src/FuctionalCalculatorBiFunction/FunctionalCalculator.java
+++ src/FuctionalCalculatorBiFunction/FunctionalCalculator.java
@@ -1,4 +1,6 @@
 package FuctionalCalculatorBiFunction;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.function.BiFunction;
 
@@ -15,6 +17,7 @@ public class FunctionalCalculator {
         BiFunction<Integer,Integer,Integer> mult = (a,b) -> a * b;
         BiFunction<Integer,Integer,Integer> div = (a,b) -> a/b;
 
+
         Scanner user_input = new Scanner(System.in);
         System.out.println("enter number one:");
         int number1= user_input.nextInt();
@@ -22,18 +25,29 @@ public class FunctionalCalculator {
         System.out.println("enter number two:");
         int number2= user_input.nextInt();
 
-        if (number1 !=0 && number2 !=0){
+        if (number2 !=0){
 
             int result1 = FunctionalCalculator.calculate(number1,number2,add);
             int result2 = FunctionalCalculator.calculate(number1,number2,sub);
             int result3 = FunctionalCalculator.calculate(number1,number2,mult);
             int result4 = FunctionalCalculator.calculate(number1,number2,div);
 
+            Map<String,Integer> result = Map.of("Addition",FunctionalCalculator.calculate(number1,number2,add),
+                    "subtraction",FunctionalCalculator.calculate(number1,number2,sub),"multiplication",FunctionalCalculator.calculate(number1,number2,mult),
+                            "division", FunctionalCalculator.calculate(number1,number2,div));
+
+
+
 
             System.out.println("Addition:" + " " + number1 + " " + " " + "+" +  " " + number2 + " " + "="+ " " + result1);
             System.out.println("Subtraction:" + " " + number1 + " " + " " + "-" +  " " + number2 + " " + "="+ " " + result2);
             System.out.println("Multiplication:" + " " + number1 + " " + " " + "*" +  " " + number2 + " " + "="+ " " + result3);
             System.out.println("Division:" + " " + number1 + " " + " " + "/" +  " " + number2 + " " + "="+ " " + result4);
+            System.out.println("--------------Map Format Output--------------");
+
+            for(Map.Entry<String,Integer> entry : result.entrySet()){
+                System.out.println(entry.getKey() + "=" + entry.getValue());
+            }
 
         }else{
             System.out.println("please enter number not equal to 0");
