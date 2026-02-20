9c4e3ad66d8affb72cb89b420c80b55e122632d5
nishimwecelie <nishimwecelie@gmail.com>
2026-02-20 12:45:07 +0200

    filtering list by even numbers done

---
+++ src/FilterNumbers/EvenNumberFilter.java
@@ -0,0 +1,24 @@
+package FilterNumbers;
+
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EvenNumberFilter {
+    public static List<Integer> filterEvenNumbers(List<Integer> a){
+
+        List<Integer> result = a.stream().filter(b -> b%2 == 0).collect(Collectors.toList());
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        List<Integer> allNumbers = List.of(1,2,3,4,5,6,7,8,9,10);
+        List<Integer> filter = filterEvenNumbers(allNumbers);
+
+
+        System.out.println("List before Filtering:" + allNumbers);
+        System.out.println("List after Filtering:" + filter);
+
+    }
+}
